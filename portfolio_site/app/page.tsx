a8f47a12d1700453f320e6677179b01536ed9fa4
FimbulWinters <dakin.92@outlook.com>
2023-01-09 11:12:23 +0000

    Changing file and routing structure

--- portfolio_site/app/page.tsx
+++ portfolio_site/app/page.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import Profile from "./profile";
+import Technology from "./technology";
+import Top from "./top";
 
-function Home() {
-  return <div>I'm Home</div>;
+export default function Home() {
+  return (
+    <section className=" h-screen bg-background">
+      <body className="h-4/5 bg-background">
+        <Top />
+        <Profile />
+        <Technology />
+      </body>
+      <footer className="absolute bottom-0"></footer>
+    </section>
+  );
 }
-
-export default Home;
